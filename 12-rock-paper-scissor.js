3474bc2292b28b40bf2d3533c542fc34d9f82249
arujsiddiqui <arujsiddiqui22@gmail.com>
2026-02-25 12:29:04 +0530

    Update 12-rock-paper-scissor.js

--- 12-rock-paper-scissor.js
+++ 12-rock-paper-scissor.js
@@ -116,8 +116,8 @@ let intervalId;
     document.querySelector('.js-result').innerHTML = result;
 
     document.querySelector('.js-move').innerHTML = ` You
-    <img src="${playerMove}-emoji.png" class="move-icon">
-    <img src="${computerMove}-emoji.png" class="move-icon">
+    <img src="./${playerMove}-emoji.png" class="move-icon">
+    <img src="./${computerMove}-emoji.png" class="move-icon">
     Computer`;
      }
 
@@ -141,3 +141,4 @@ let intervalId;
     return computerMove;     
 
     }
+
